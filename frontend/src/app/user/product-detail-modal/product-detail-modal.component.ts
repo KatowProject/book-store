dced19b829c968711045d363b84a8c9829cbb126
KatowProject <naufalfaqih443@gmail.com>
2023-05-29 05:48:27 +0700

    partial update

--- frontend/src/app/user/product-detail-modal/product-detail-modal.component.ts
+++ frontend/src/app/user/product-detail-modal/product-detail-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController, ToastController } from '@ionic/angular';
+import { FolderPage } from 'src/app/folder/folder.page';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-product-detail-modal',
@@ -9,7 +11,7 @@ import { ModalController } from '@ionic/angular';
 export class ProductDetailModalComponent implements OnInit {
   @Input() item: any;
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private toastController: ToastController, private alertController: AlertController, private folderPage: FolderPage) { }
 
   ngOnInit() {
     console.log(this.item);
@@ -19,5 +21,39 @@ export class ProductDetailModalComponent implements OnInit {
     this.modalController.dismiss();
   }
 
-  addToCart(item: any) { }
+  async addToCart(item: any) {
+    try {
+      const res = await fetch(environment.BASE_URL + 'api/add-to-cart', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+          product_id: item.id,
+          quantity: 1
+        })
+      });
+
+      const data = await res.json();
+      if (data.statusCode === 200) {
+        this.toastController.create({
+          message: 'Berhasil menambahkan ke keranjang',
+          duration: 2000,
+        }).then(toast => {
+          toast.present();
+        });
+
+        this.folderPage.getCartLength();
+      }
+    } catch (err: any) {
+      this.alertController.create({
+        header: 'Error',
+        message: err.message,
+        buttons: ['OK']
+      }).then(alert => {
+        alert.present();
+      });
+    }
+  }
 }
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: 'http://localhost:8000/'
+  BASE_URL: 'https://bookstore.mengkodingkan.dev/'
 };
